6266a16fbbde290b9b86cf01065b66651f5aaf22
Juan Pablo Guevara <guevara_juan1990@hotmail.com>
2022-07-09 16:04:16 -0500

    add styles on app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			<h1>Curso de Programacion Web</h1>
+			<h1 className="TeresaStyle">Curso de Programacion Web</h1>
 			<h2>Temario: </h2>
 			<ol>
 				<li>Html</li>
